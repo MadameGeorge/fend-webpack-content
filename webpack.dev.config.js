54ab121f7edff1d4c6386c0386bdb6bed6ee3a30
MadameGeorge <magda.powalisz@gmail.com>
2020-06-07 13:26:59 +0100

    feat: Add production and development modes in webpack config

---
+++ webpack.dev.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/client/index.js',
+	module: {
+		rules: [
+			{
+				test: '/\.js$/',
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
+				enforce: 'pre',
+				test: '/\.js$/',
+				exclude: /node_modules/,
+				loader: 'eslint-loader'
+			}
+		]
+	},
+	plugins: [
+		new htmlWebpackPlugin({
+			template: './src/client/views/index.html',
+			filename: './index.html',
+		})
+	]
+};
